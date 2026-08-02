f043aabeb1f80a5c24655fc36b13e85f730c570d
DwnNyxDev <vatelvandell@gmail.com>
2026-08-01 22:10:11 -0400

    Project Structure + Initial Progress

---
+++ arduino/tests/motor_driver_test/motor_driver_test.ino
@@ -0,0 +1,34 @@
+#define STBY 8
+#define AIN1 9
+#define AIN2 10
+#define PWMA 5
+#define BIN1 7
+#define BIN2 12
+#define PWMB 6
+
+void setup() {
+  pinMode(STBY, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  
+  digitalWrite(STBY, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA, 150);
+
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+  analogWrite(PWMB, 150);
+
+  delay(2000);
+
+  analogWrite(PWMA, 0);
+  analogWrite(PWMB, 0);
+  delay(2000);
+}
